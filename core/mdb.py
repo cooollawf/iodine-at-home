50fc351855a1e236bdc22a8a5b20fcb8b501e531
Dongyanmio <dongyanmio@outlook.com>
2024-10-06 19:48:12 +0800

    Revert "refactor: 重构 MongoDB 数据格式"
    This reverts commit 4b8546b0ab44193a2d5709d62c55637de7db0fe7.

--- core/mdb.py
+++ core/mdb.py
@@ -107,11 +107,9 @@ class Database:
             "port": port,
             "version": version,
             "runtime": runtime,
-            "ssl": {
-                "cert": ssl_cert,
-                "key": ssl_key,
-                "expiry": ssl_expiry
-            }
+            "ssl_cert": ssl_cert,
+            "ssl_key": ssl_key,
+            "ssl_expiry": ssl_expiry
         }
         valid_update_data = {k: v for k, v in data.items() if v is not None}
         result = await self.db.clusters.update_one(
